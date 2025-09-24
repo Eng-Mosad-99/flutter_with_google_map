9a8b4ffb80ce619f807b2e7ad374e49630e44ae5
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-09-24 14:03:42 +0300

    update camera and update LatLng

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -24,24 +24,52 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     super.initState();
   }
 
+  late GoogleMapController googleMapController;
+  @override
+  void dispose() {
+    googleMapController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: initialCameraPosition,
-        cameraTargetBounds: CameraTargetBounds(
-          LatLngBounds(
-            northeast: const LatLng(
-              31.437220202959264,
-              31.547018440591337,
-            ),
-            southwest: const LatLng(
-              30.607313478445793,
-              31.2634743280193,
+    return Stack(
+      children: [
+        GoogleMap(
+          onMapCreated: (controller) {
+            googleMapController = controller;
+          },
+          initialCameraPosition: initialCameraPosition,
+          cameraTargetBounds: CameraTargetBounds(
+            LatLngBounds(
+              northeast: const LatLng(
+                31.437220202959264,
+                31.547018440591337,
+              ),
+              southwest: const LatLng(
+                30.607313478445793,
+                31.2634743280193,
+              ),
             ),
           ),
         ),
-      ),
+        Positioned(
+          right: 16,
+          left: 16,
+          bottom: 16,
+          child: ElevatedButton(
+            onPressed: () {
+          
+              googleMapController.animateCamera(
+                CameraUpdate.newLatLng(const LatLng(30.764166086674447, 31.32060310583492)),
+              );
+            },
+            child: const Text(
+              'Change Location',
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
