b0f0c6865769ecf2997abdb36c4395562113440a
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-09-24 13:31:12 +0300

    add camera bounds

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CustomGoogleMap extends StatefulWidget {
-  const CustomGoogleMap({super.key});
+  const CustomGoogleMap({
+    super.key,
+  });
 
   @override
   State<CustomGoogleMap> createState() => _CustomGoogleMapState();
@@ -10,12 +12,14 @@ class CustomGoogleMap extends StatefulWidget {
 
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition initialCameraPosition;
-
   @override
   void initState() {
     initialCameraPosition = const CameraPosition(
-      target: LatLng(31.1726894983781, 31.515860310085735),
-      zoom: 14.0,
+      target: LatLng(
+        31.1726894983781,
+        31.515860310085735,
+      ),
+      zoom: 12,
     );
     super.initState();
   }
@@ -25,6 +29,18 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     return Scaffold(
       body: GoogleMap(
         initialCameraPosition: initialCameraPosition,
+        cameraTargetBounds: CameraTargetBounds(
+          LatLngBounds(
+            northeast: const LatLng(
+              31.437220202959264,
+              31.547018440591337,
+            ),
+            southwest: const LatLng(
+              30.607313478445793,
+              31.2634743280193,
+            ),
+          ),
+        ),
       ),
     );
   }
