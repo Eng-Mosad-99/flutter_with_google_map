5a0288a53404f7a45902ceeccba9efbba760600f
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-09-24 13:06:52 +0300

    add zoom level sizes

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -30,8 +30,3 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
 }
 
 
-// world view => 0 ----> 3
-// country view => 4 ----> 6
-// city view => 10 ----> 12
-// street view => 13 ----> 17
-// building view => 18 -----> 20
---
+++ lib/widgets/zoom_level_sizes.dart
@@ -0,0 +1,5 @@
+// world view => 0 ----> 3
+// country view => 4 ----> 6
+// city view => 10 ----> 12
+// street view => 13 ----> 17
+// building view => 18 -----> 20
